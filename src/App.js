926eb5df0eac8fd88d8eac0bc8decc4e9d0998cb
AsSchneider <martinbotta85@gmail.com>
2022-12-03 15:45:33 -0300

    prueba2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
-
+import Cart from "./components/Cart";
 import Contacto from "./components/Contacto";
 import Footer from './components/Footer';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import Navbar from './components/Navbar';
+import CartProvider from "./components/CartContext";
 
 function App() {
   return (
+    <CartProvider>
     <BrowserRouter>
       <Navbar/>
       <Routes>
@@ -17,10 +20,12 @@ function App() {
         <Route path='/contacto' element={<Contacto/>} />
         <Route path='/categoria/:idcategoria' element={<ItemListContainer/>} />
         <Route path='/item/:iditem' element={<ItemDetailContainer/>} />
+        <Route path="/cart" element={<Cart />} />
         </Routes>
       <Footer/>
 
     </BrowserRouter>
+    </CartProvider>
   );
 }
 
